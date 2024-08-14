8f0ce6f4c5380ed30fbab495472901b5ec9cc7a6
vinsen_wijaya <36811691+vwjaya32@users.noreply.github.com>
2024-08-15 02:33:38 +0700

    setup MongoDB + simple http server

--- README.md
+++
@@ -1,1 +0,0 @@
-# hangry-case-study
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,47 @@
-console.log("Hello");
+import { config } from 'dotenv';
+import { MongoClient, ServerApiVersion } from 'mongodb';
+import http from 'http';
+
+config();
+// Connect to Database
+const uri = process.env.DB_URL;
+if (!uri) {
+    throw new Error('URL Database tidak ditemukan');
+}
+
+const db = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function connectToDB() {
+    try {
+      await db.connect();
+      await db.db("admin").command({ ping: 1 });
+      console.log("Database telah terhubung");
+    } finally {
+      await db.close();
+    }
+}
+connectToDB();
+
+
+const requestListener = (
+    request: http.IncomingMessage, 
+    response: http.ServerResponse): void => {
+    response.setHeader('Content-Type', 'text/plain');
+    response.statusCode = 200;
+    response.end('Selamat Datang');
+};
+
+const server = http.createServer(requestListener);
+ 
+const port: number = parseInt(process.env.PORT ?? '5001', 10);
+const host: string = 'localhost';
+ 
+server.listen(port, host, () => {
+    console.log(`Server berjalan pada http://${host}:${port}`);
+});
